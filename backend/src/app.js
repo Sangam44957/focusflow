88683fdf454375d18915e433a5c446c0674c8dcc
sangam <sangammehta44@gmail.com>
2025-12-14 20:42:35 +0530

    fix: add PATCH method to CORS allowed methods

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(helmet());
 app.use(cors({
   origin: [config.frontendUrl, 'https://focusflow-drab.vercel.app', 'http://localhost:3000'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
